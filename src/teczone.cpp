0067ed56f09f6172b97c120fb53a6eb06b673b57
David Nelles <easyprogramms@gmail.com>
2019-03-23 00:47:31 +0100

    implemented TecZone data setter

--- src/teczone.cpp
+++ src/teczone.cpp
@@ -1,6 +1,118 @@
 #include "teczone.h"
 
-TecZone::TecZone()
+TecZone::TecZone() :
+    m_dimension(0)
 {
 
+}
+
+bool
+TecZone::setData(QMap<QString, QVector<double>> values)
+{
+    //check if TecZone is alredy used
+    if(m_dimension != 0)
+        return false;
+
+    //check if all vectors in values have the same length
+    if (!chekForConsistence(values))
+        return false;
+
+    m_dimension = 1;
+
+    m_1dData = values;
+
+    return true;
+}
+
+bool
+TecZone::setData(QMap<QString, QVector<double>> values, int firstAxisLength)
+{
+    //check if TecZone is alredy used
+    if(m_dimension != 0)
+        return false;
+
+    //check if all vectors in values have the same length
+    int vectorLength = chekForConsistence(values);
+    if(!vectorLength)
+        return false;
+
+    //check if length of the vectors is divisible integer
+    if(vectorLength / firstAxisLength != double(vectorLength) / double(firstAxisLength))
+        return false;
+
+    m_dimension = 2;
+
+    foreach(QString param, values.keys())
+    {
+        QVector<double> firstAxisData;
+        for(int i = 0; i < vectorLength; i++)
+        {
+            firstAxisData << values[param][i];
+
+            if(firstAxisData.length() == firstAxisLength)
+            {
+                m_2dData[param] << firstAxisData;
+                firstAxisData.clear();
+            }
+        }
+    }
+
+    return true;
+}
+
+bool
+TecZone::setData(QMap<QString, QVector<double>> values, int firstAxisLength, int seccondAxisLength)
+{
+    //check if TecZone is alredy used
+    if(m_dimension != 0)
+        return false;
+
+    //check if all vectors in values have the same length
+    int vectorLength = chekForConsistence(values);
+    if(!vectorLength)
+        return false;
+
+    //check if length of the vectors is divisible integer
+    if(vectorLength / (firstAxisLength * seccondAxisLength) != double(vectorLength) / double(firstAxisLength * seccondAxisLength))
+        return false;
+
+    m_dimension = 3;
+
+    foreach(QString param, values.keys())
+    {
+        QVector<double> firstAxisData;
+        QVector<QVector<double>> seccondAxisData;
+        for(int i = 0; i < vectorLength; i++)
+        {
+            firstAxisData << values[param][i];
+
+            if(firstAxisData.length() == firstAxisLength)
+            {
+                seccondAxisData << firstAxisData;
+                firstAxisData.clear();
+            }
+
+            if(seccondAxisData.length() == seccondAxisLength)
+            {
+                m_3dData[param] << seccondAxisData;
+                seccondAxisData.clear();
+            }
+        }
+    }
+
+    return true;
+}
+
+int
+TecZone::chekForConsistence(QMap<QString, QVector<double>> input)
+{
+    int refLength = input[input.keys().first()].length();
+
+    foreach (QString e, input.keys())
+    {
+        if(input[e].length() != refLength)
+            return false;
+    }
+
+    return refLength;
 }
--- src/teczone.h
+++ src/teczone.h
@@ -2,13 +2,29 @@
 #define TECZONE_H
 
 #include <QObject>
+#include <QMap>
+#include <QVector>
+#include <QString>
+#include <QStringList>
 
-class TecZone
+class TecZone : public QObject
 {
+    Q_OBJECT
 
 public:
     TecZone();
+    bool setData(QMap<QString, QVector<double>> values);
+    bool setData(QMap<QString, QVector<double>> values, int firstAxisLength);
+    bool setData(QMap<QString, QVector<double>> values, int firstAxisLength, int seccondAxislength);
 
+private:
+    int m_dimension;
+
+    QMap<QString, QVector<double>> m_1dData;
+    QMap<QString, QVector<QVector<double>>> m_2dData;
+    QMap<QString, QVector<QVector<QVector<double>>>> m_3dData;
+
+    int chekForConsistence(QMap<QString, QVector<double>> input);
 };
 
 #endif // TECZONE_H
