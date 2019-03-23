77a07b816c45a0426b9434b723ad1c6ba0ca85a3
David Nelles <easyprogramms@gmail.com>
2019-03-23 15:43:19 +0100

    Added some valueGetters

--- src/teczone.cpp
+++ src/teczone.cpp
@@ -14,7 +14,7 @@ TecZone::setData(QMap<QString, QVector<double>> values)
         return false;
 
     //check if all vectors in values have the same length
-    if (!chekForConsistence(values))
+    if (!checkForConsistence(values))
         return false;
 
     m_dimension = 1;
@@ -32,7 +32,7 @@ TecZone::setData(QMap<QString, QVector<double>> values, int firstAxisLength)
         return false;
 
     //check if all vectors in values have the same length
-    int vectorLength = chekForConsistence(values);
+    int vectorLength = checkForConsistence(values);
     if(!vectorLength)
         return false;
 
@@ -68,7 +68,7 @@ TecZone::setData(QMap<QString, QVector<double>> values, int firstAxisLength, int
         return false;
 
     //check if all vectors in values have the same length
-    int vectorLength = chekForConsistence(values);
+    int vectorLength = checkForConsistence(values);
     if(!vectorLength)
         return false;
 
@@ -103,8 +103,148 @@ TecZone::setData(QMap<QString, QVector<double>> values, int firstAxisLength, int
     return true;
 }
 
+bool
+TecZone::setData(QMap<QString, QVector<QVector<double>>> values)
+{
+    //check if TecZone is alredy used
+    if(m_dimension != 0)
+        return false;
+
+    //check if all vectors in values have the same length
+/*    int vectorLength = checkForConsistence(values);
+    if(!vectorLength)
+        return false;*/
+
+    m_2dData = values;
+
+    return true;
+}
+
+bool
+TecZone::setData(QMap<QString, QVector<QVector<QVector<double>>>> values)
+{
+    //check if TecZone is alredy used
+    if(m_dimension != 0)
+        return false;
+
+/*    //check if all vectors in values have the same length
+    int vectorLength = checkForConsistence(values);
+    if(!vectorLength)
+        return false;*/
+
+    m_3dData = values;
+
+    return true;
+}
+
+
+double
+TecZone::data0D(QString param, int iAxisPosition) //if dimension == 1
+{
+    if(m_dimension != 1)
+        return NAN;
+
+    return m_1dData[param][iAxisPosition];
+}
+
+double
+TecZone::data0D(QString param, int iAxisPosition, int jAxisPosition) //if dimension == 2
+{
+    if(m_dimension != 2)
+        return NAN;
+
+    return m_2dData[param][jAxisPosition][iAxisPosition];
+}
+
+double
+TecZone::data0D(QString param, int iAxisPosition, int jAxisPosition, int kAxisPosition) //if dimension == 3
+{
+    if(m_dimension != 1)
+        return NAN;
+
+    return m_3dData[param][kAxisPosition][jAxisPosition][iAxisPosition];
+}
+
+QVector<double>
+TecZone::data1D(QString param) //if dimension == 1
+{
+    if(m_dimension != 1)
+        return QVector<double>();
+
+    return m_1dData[param];
+}
+
+QVector<double>
+TecZone::data1D(QString param, int axisPosition, Axis returnAxis) //if dimension == 2
+{
+    if(m_dimension != 2)
+        return QVector<double>();
+
+    if(returnAxis == Axis::I)
+    {
+        return m_2dData[param][axisPosition];
+    }
+    else
+    {
+        QVector<double> retval;
+
+        foreach (QVector<double> jLine, m_2dData[param])
+        {
+            retval << jLine[axisPosition];
+        }
+
+        return retval;
+    }
+}
+
+QVector<double>
+TecZone::data1D(QString param, int firstAxisPosition, int seccondAxisPosition, Axis returnAxis) //if dimension == 3
+{
+    if(m_dimension != 3)
+        return QVector<double>();
+
+    if(returnAxis == Axis::I)
+    {
+        return m_3dData[param][seccondAxisPosition][firstAxisPosition];
+    }
+    else if(returnAxis == Axis::J)
+    {
+        QVector<double> retval;
+        foreach (QVector<double> jLine, m_3dData[param][seccondAxisPosition])
+        {
+            retval << jLine[firstAxisPosition];
+        }
+
+        return retval;
+    }
+    else
+    {
+        QVector<double> retval;
+        foreach (QVector<QVector<double>> kSlice, m_3dData[param])
+        {
+            retval << kSlice[seccondAxisPosition][firstAxisPosition];
+        }
+
+        return retval;
+    }
+}
+
+QVector<QVector<double>> data2D(QString param); //if dimension == 2
+QVector<QVector<double>> data2D(QString param, int iaxisPosition); //if dimension == 3
+QVector<QVector<QVector<double>>> data3D(QString param); //dimenion = 3
+
+
+
+
+
 int
-TecZone::chekForConsistence(QMap<QString, QVector<double>> input)
+TecZone::dimension()
+{
+    return m_dimension;
+}
+
+int
+TecZone::checkForConsistence(QMap<QString, QVector<double>> input)
 {
     int refLength = input[input.keys().first()].length();
 
--- src/teczone.h
+++ src/teczone.h
@@ -10,21 +10,85 @@
 class TecZone : public QObject
 {
     Q_OBJECT
+    Q_ENUMS(Axis)
 
 public:
+    enum Axis {I, J, K};
+
     TecZone();
     bool setData(QMap<QString, QVector<double>> values);
     bool setData(QMap<QString, QVector<double>> values, int firstAxisLength);
     bool setData(QMap<QString, QVector<double>> values, int firstAxisLength, int seccondAxislength);
+    bool setData(QMap<QString, QVector<QVector<double>>> values);
+    bool setData(QMap<QString, QVector<QVector<QVector<double>>>> values);
+
+    /**
+     * @brief To get a value of an 1D-Zone
+     * @param param
+     * @param iAxisPosition
+     * @return value of an 1D-Zone
+     */
+    double data0D(QString param, int iAxisPosition); //if dimension == 1
+
+    /**
+     * @brief To get a value of an 2D-Zone
+     * @param param
+     * @param iAxisPosition
+     * @param jAxisPosition
+     * @return value of an 2D-Zone
+     */
+    double data0D(QString param, int iAxisPosition, int jAxisPosition); //if dimension == 2
+
+    /**
+     * @brief To get a value of an 3D-Zone
+     * @param param
+     * @param iAxisPosition
+     * @param jAxisPosition
+     * @param kAxisPosition
+     * @return value of an 3D-Zone
+     */
+    double data0D(QString param, int iAxisPosition, int jAxisPosition, int kAxisPosition); //if dimension == 3
+
+    /**
+     * @brief Get whole 1D-Data as Vector
+     * @param param
+     * @return 1D-Data as Vector
+     */
+    QVector<double> data1D(QString param);  //if dimension == 1
+
+    /**
+     * @brief Get 1D-Slice from 2D-Data as Vector
+     * @param param
+     * @param axisPosition
+     * @param returnAxis
+     * @return 1D-Slice as Vector
+     */
+    QVector<double> data1D(QString param, int axisPosition, Axis returnAxis); //if dimension == 2
+
+    /**
+     * @brief Get 1D-Slice from 3D-Data as Vector
+     * @param param
+     * @param firstAxisPosition = if returnAxis==I : J else I
+     * @param seccondAxisPosition = if returnAxis==K : J else K
+     * @param returnAxis
+     * @return 1D-Slice as Vector
+     */
+    QVector<double> data1D(QString param, int firstAxisPosition, int seccondAxisPosition, Axis returnAxis); //if dimension == 3
+
+    QVector<QVector<double>> data2D(QString param); //if dimension == 2
+    QVector<QVector<double>> data2D(QString param, int iaxisPosition); //if dimension == 3
+    QVector<QVector<QVector<double>>> data3D(QString param); //dimenion = 3
+
+    int dimension();
 
 private:
     int m_dimension;
 
-    QMap<QString, QVector<double>> m_1dData;
-    QMap<QString, QVector<QVector<double>>> m_2dData;
-    QMap<QString, QVector<QVector<QVector<double>>>> m_3dData;
+    QMap<QString, QVector<double>> m_1dData; //[i]
+    QMap<QString, QVector<QVector<double>>> m_2dData; //[j][i]
+    QMap<QString, QVector<QVector<QVector<double>>>> m_3dData; //[k][j][i]
 
-    int chekForConsistence(QMap<QString, QVector<double>> input);
+    int checkForConsistence(QMap<QString, QVector<double>> input);
 };
 
 #endif // TECZONE_H
